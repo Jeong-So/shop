6dd4f5e77db74406476b070dfbb2025216bb8a1a
Jeong-So <83945245+Jeong-So@users.noreply.github.com>
2022-11-30 23:32:43 +0900

    (20221130) 상품 Repository 테스트 코드 학습 및 Spring Data JPA의 JpaRepository 및 상속구조 학습

--- src/test/java/com/jeong/shop/repository/ItemRepositoryTest.java
+++ src/test/java/com/jeong/shop/repository/ItemRepositoryTest.java
@@ -12,15 +12,15 @@ import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
-@TestPropertySource(locations="classpath:application-test.properties")
+@SpringBootTest  // 통합 테스트를 위해 스프링 부트에서 제공하는 어노테이션, 실제 어플리케이션을 구동할 때처럼 모든 Bean을 IoC 컨테이너에 등록
+@TestPropertySource(locations="classpath:application-test.properties")  // 설정 파일 application-test.properties에 우선순위 부여 : Test DB로 H2를 사용
 class ItemRepositoryTest {
 
-    @Autowired
+    @Autowired   // itemRepository를 사용하기 위하여 @Autowired 어노테이션을 이용하여 Bean 주입
     ItemRepository itemRepository;
 
-    @Test
-    @DisplayName("상품 저장 테스트")
+    @Test   // 테스트할 대상 지정 : 테스트할 메소드 위에 선언
+    @DisplayName("상품 저장 테스트")   // Junit5에 추가된 어노테이션으로 테스트 코드 실행 시 @DisplayName에 지정한 테스트명 노출
     public void createItemTest() {
         ItemEntity item = new ItemEntity();
         item.setItemName("테스트 상품");
@@ -30,7 +30,7 @@ class ItemRepositoryTest {
         item.setItemSellStatus(ItemSellStatus.SELL);
         item.setRegTime(LocalDateTime.now());
         item.setUpdateTime(LocalDateTime.now());
-        ItemEntity savedItem = itemRepository.save(item);
+        ItemEntity savedItem = itemRepository.save(item);  // itemRepository가 상속받은 JpaRepository(CrudRepository) 인터페이스의 save 메소드 동작
         System.out.println(savedItem.toString());
     }
 
