6883936cf9de8941e9f29943ac18330ac2421b23
Jeong-So <83945245+Jeong-So@users.noreply.github.com>
2022-12-02 00:59:13 +0900

    (20221201) JPA 쿼리 메소드 및 @Query 사용법 실습 및 테스트

--- src/main/java/com/jeong/shop/repository/ItemRepository.java
+++ src/main/java/com/jeong/shop/repository/ItemRepository.java
@@ -2,7 +2,41 @@ package com.jeong.shop.repository;
 
 import com.jeong.shop.entity.ItemEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 // JpaRepository에는 기본적인 CRUD 및 페이징 처리를 위한 메소드 정의
 public interface ItemRepository extends JpaRepository<ItemEntity, Long> {
+    // #1 쿼리 메소드 실습
+    // 쿼리 메소드를 이용한 상품 조회 실습
+    List<ItemEntity> findByItemName(String itemName);
+
+    // 상품명, 상품상세설명 OR 조건 실습
+    List<ItemEntity> findByItemNameOrItemDetail(String itemNm, String itemDetail);
+
+    // 상품명, 상품상세설명 And 조건 실습
+    List<ItemEntity> findByItemNameAndItemDetail(String itemNm, String itemDetail);
+
+    // 가격 LessThan 실습
+    List<ItemEntity> findByPriceLessThan(Integer price);
+
+    // 가격 LessThan 및 가격 내림차순 조회 실습
+    List<ItemEntity> findByPriceLessThanOrderByPriceDesc(Integer price);
+
+
+    // #2 @Query 어노테이션 실습
+    // @Query 상품 데이터 조회 실습
+    @Query("select i from ItemEntity i where i.itemDetail like %:itemDetail% " +
+            "order by i.price desc")
+    List<ItemEntity> findByItemDetail(@Param("itemDetail") String itemDetail);
+
+    // @Query-nativeQuery를 이용한 상품 조회 실습
+    // nativeQuery : 기존의 데이터베이스에서 사용하던 쿼리를 그대로 사용해야 할 때 사용
+    // But, 특정 데이터베이스에 종속됨
+    @Query(value = "select * from item i where i.item_detail like %:itemDetail% " +
+            "order by i.price desc", nativeQuery = true)
+    List<ItemEntity> findByItemDetailByNative(@Param("itemDetail") String itemDetail);
+
 }
--- src/test/java/com/jeong/shop/repository/ItemRepositoryTest.java
+++ src/test/java/com/jeong/shop/repository/ItemRepositoryTest.java
@@ -9,8 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
 import java.time.LocalDateTime;
-
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
 
 @SpringBootTest  // 통합 테스트를 위해 스프링 부트에서 제공하는 어노테이션, 실제 어플리케이션을 구동할 때처럼 모든 Bean을 IoC 컨테이너에 등록
 @TestPropertySource(locations="classpath:application-test.properties")  // 설정 파일 application-test.properties에 우선순위 부여 : Test DB로 H2를 사용
@@ -22,16 +21,101 @@ class ItemRepositoryTest {
     @Test   // 테스트할 대상 지정 : 테스트할 메소드 위에 선언
     @DisplayName("상품 저장 테스트")   // Junit5에 추가된 어노테이션으로 테스트 코드 실행 시 @DisplayName에 지정한 테스트명 노출
     public void createItemTest() {
-        ItemEntity item = new ItemEntity();
-        item.setItemName("테스트 상품");
-        item.setPrice(10000);
-        item.setStockNumber(100);
-        item.setItemDetail("테스트 상품 상세 설명");
-        item.setItemSellStatus(ItemSellStatus.SELL);
-        item.setRegTime(LocalDateTime.now());
-        item.setUpdateTime(LocalDateTime.now());
-        ItemEntity savedItem = itemRepository.save(item);  // itemRepository가 상속받은 JpaRepository(CrudRepository) 인터페이스의 save 메소드 동작
-        System.out.println(savedItem.toString());
+        for(int i=1; i<=10; i++) {
+            ItemEntity item = new ItemEntity();
+            item.setItemName("테스트 상품"+ i );
+            item.setPrice(10000+ i);
+            item.setStockNumber(100 - i);
+            item.setItemDetail("테스트 상품 상세 설명" + i);
+            item.setItemSellStatus(ItemSellStatus.SELL);
+            item.setRegTime(LocalDateTime.now());
+            item.setUpdateTime(LocalDateTime.now());
+            ItemEntity savedItem = itemRepository.save(item);  // itemRepository가 상속받은 JpaRepository(CrudRepository) 인터페이스의 save 메소드 동작
+            System.out.println(savedItem.toString());
+        }
     }
+
+    // #1 쿼리 메소드 테스트
+    // 쿼리 메소드를 이용한 상품 조회 테스트
+    @Test
+    @DisplayName("상품명 조회 테스트")
+    public void findByItemNameTest() {
+        this.createItemTest(); // 10개의 상품 저장하는 테스트코드 실행
+        List<ItemEntity> itemList = itemRepository.findByItemName("테스트 상품3");
+        System.out.println("Test");
+        for(ItemEntity itemEntity : itemList) {
+            System.out.println(itemEntity.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("상품명, 상품상세설명 or 테스트")
+    public void findByItemNameOrItemDetailTest(){
+        this.createItemTest(); // 10개의 상품 저장하는 테스트코드 실행
+        List<ItemEntity> itemList = itemRepository.findByItemNameOrItemDetail("테스트 상품2", "테스트 상품 상세 설명5");
+        System.out.println("Test");
+        for(ItemEntity itemEntity : itemList) {
+            System.out.println(itemEntity.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("상품명, 상품상세설명 And 테스트")
+    public void findByItemNameAndItemDetailTest(){
+        this.createItemTest(); // 10개의 상품 저장하는 테스트코드 실행
+        List<ItemEntity> itemList = itemRepository.findByItemNameAndItemDetail("테스트 상품2", "테스트 상품 상세 설명5");
+        System.out.println("Test");
+        for(ItemEntity itemEntity : itemList) {
+            System.out.println(itemEntity.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("가격 LessThan 테스트")
+    public void findByPriceLessThanTest(){
+        this.createItemTest(); // 10개의 상품 저장하는 테스트코드 실행
+        List<ItemEntity> itemList = itemRepository.findByPriceLessThan(10004);
+        System.out.println("Test");
+        for(ItemEntity itemEntity : itemList) {
+            System.out.println(itemEntity.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("가격 내림차순 조회 테스트")
+    public void findByPriceLessThanOrderByPriceDescTest(){
+        this.createItemTest(); // 10개의 상품 저장하는 테스트코드 실행
+        List<ItemEntity> itemList = itemRepository.findByPriceLessThanOrderByPriceDesc(10004);
+        System.out.println("Test");
+        for(ItemEntity itemEntity : itemList) {
+            System.out.println(itemEntity.toString());
+        }
+    }
+
+    // #2 @Query 테스트
+    // @Query를 이용한 상품 조회 테스트
+    @Test
+    @DisplayName("@Query를 이용한 상품 조회 테스트")
+    public void findByItemDetailTest(){
+        this.createItemTest(); // 10개의 상품 저장하는 테스트코드 실행
+        List<ItemEntity> itemList = itemRepository.findByItemDetail("테스트 상품 상세 설명");
+        System.out.println("Test");
+        for(ItemEntity itemEntity : itemList) {
+            System.out.println(itemEntity.toString());
+        }
+    }
+
+    // @Query-nativeQuery를 이용한 상품 조회 테스트
+    @Test
+    @DisplayName("@Query-nativeQuery를 이용한 상품 조회 테스트")
+    public void findByItemDetailByNativeTest(){
+        this.createItemTest(); // 10개의 상품 저장하는 테스트코드 실행
+        List<ItemEntity> itemList = itemRepository.findByItemDetailByNative("테스트 상품 상세 설명");
+        System.out.println("Test");
+        for(ItemEntity itemEntity : itemList) {
+            System.out.println(itemEntity.toString());
+        }
+    }
+
 
 }
