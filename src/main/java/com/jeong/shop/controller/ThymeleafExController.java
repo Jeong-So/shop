ab2d9818edec6af3fc4753ac3bffb7335c90fc95
Jeong-So <83945245+Jeong-So@users.noreply.github.com>
2022-12-21 23:21:40 +0900

    (20221221) Thymeleaf 실습 진행2 : th:each / th:if th:unless / th:switch th:case / th:href

--- src/main/java/com/jeong/shop/controller/ThymeleafExController.java
+++ src/main/java/com/jeong/shop/controller/ThymeleafExController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping(value="/thymeleaf")
@@ -23,6 +25,7 @@ public class ThymeleafExController {
     }
 
     // thymeleaf th:text 예제
+    // 모델에 데이터를 담아서 뷰에 전달
     @GetMapping(value = "/ex02")
     public String thymeleafExample02(Model model){
         ItemDto itemDto = new ItemDto();
@@ -33,5 +36,57 @@ public class ThymeleafExController {
 
         model.addAttribute("itemDto", itemDto);
         return "thymeleafEx/thymeleafEx02";
+    }
+
+    // thymeleaf th:each (반복문) 예제
+    @GetMapping(value = "/ex03")
+    public String thymeleafExample03(Model model){
+
+        List<ItemDto> itemDtoList = new ArrayList<>();
+
+        for(int i=1; i<=10 ; i++) {
+            ItemDto itemDto = new ItemDto();
+            itemDto.setItemDetail("상품 상세 설명"+i);
+            itemDto.setItemName("테스트 상품"+i);
+            itemDto.setPrice(1000*i);
+            itemDto.setRegTime(LocalDateTime.now());
+
+            itemDtoList.add(itemDto);
+        }
+        model.addAttribute("itemDtoList", itemDtoList);
+        return "thymeleafEx/thymeleafEx03";
+    }
+
+    // thymeleaf th:if th:unless (조건문) / th:switch th:case (조건문) 예제
+    @GetMapping(value = "/ex04")
+    public String thymeleafExample04(Model model){
+
+        List<ItemDto> itemDtoList = new ArrayList<>();
+
+        for(int i=1; i<=10 ; i++) {
+            ItemDto itemDto = new ItemDto();
+            itemDto.setItemDetail("상품 상세 설명"+i);
+            itemDto.setItemName("테스트 상품"+i);
+            itemDto.setPrice(1000*i);
+            itemDto.setRegTime(LocalDateTime.now());
+
+            itemDtoList.add(itemDto);
+        }
+        model.addAttribute("itemDtoList", itemDtoList);
+        return "thymeleafEx/thymeleafEx04";
+    }
+
+    // thymeleaf th:href 예제 : 링크 처리
+    @GetMapping(value = "/ex05")
+    public String thymeleafExample05() {
+        return "thymeleafEx/thymeleafEx05";
+    }
+
+    // thymeleaf th:href 예제 : 링크 처리 + 파라미터 전달
+    @GetMapping(value = "/ex06")
+    public String thymeleafExample06(String param1, String param2, Model model) {
+        model.addAttribute("param1", param1);
+        model.addAttribute("param2", param2);
+        return "thymeleafEx/thymeleafEx06";
     }
 }
